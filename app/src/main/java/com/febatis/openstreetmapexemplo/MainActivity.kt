3f70729b52f4787a7fd1e1f1e5ff777bd2a5bbe9
Lucas Febatis <lfebatis@gmail.com>
2024-01-19 23:18:42 -0300

    Add viewBinding

--- app/src/main/java/com/febatis/openstreetmapexemplo/MainActivity.kt
+++ app/src/main/java/com/febatis/openstreetmapexemplo/MainActivity.kt
@@ -2,10 +2,17 @@ package com.febatis.openstreetmapexemplo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.febatis.openstreetmapexemplo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        
     }
 }
