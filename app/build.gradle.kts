7a379e9dd5e14327f7aea2fa88229a37f4fc7576
Lucas Febatis <lfebatis@gmail.com>
2024-01-21 12:33:13 -0300

    Add osmbonuspack dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,10 @@ android {
 
 dependencies {
 
+    //OpenStreetMap
+    implementation("com.github.MKergall:osmbonuspack:6.9.0")
+
+    // Standard
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
